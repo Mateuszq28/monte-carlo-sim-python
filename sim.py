2e62f08c8c9a9b8005743e0d1c237c1cef2d0329
Mati laptop Adzi <s171577@student.pg.edu.pl>
2022-04-06 13:11:27 +0200

    zmiany po labie na laptopiku

--- sim.py
+++ sim.py
@@ -1,1 +1,17 @@
-main
+import json
+
+Class Sim:
+
+    def __init__(self):
+        with open("config.cfg") as f:
+            self.config = json.load(f)
+
+        
+
+
+    def start_sim(self):
+        for i in self.gen_range(conf.):
+
+    
+
+
