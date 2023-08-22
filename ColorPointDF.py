27094173a1140bd81be0cb341496bbb56639e596
Mateusz Miler <mateuszq28@gmail.com>
2023-08-22 15:17:25 +0200

    color scheme dataframe threshold

--- ColorPointDF.py
+++ ColorPointDF.py
@@ -1,18 +1,19 @@
 from Object3D import Object3D
 import numpy as np
 import pandas as pd
+from PIL import ImageColor
 
 class ColorPointDF():
 
     threshold_const = 0.2
-    threshold_precentage = 20
-    use_threshold = "percentage"
+    threshold_quantile = 0.2
+    use_threshold = "quantile"
 
     def __init__(self):
         pass
 
     @staticmethod
-    def process_df_by_color_scheme(df, color_scheme, drop_values):
+    def process_df_by_color_scheme(df: pd.DataFrame, color_scheme, drop_values):
 
         # Drop values (for example 0.0)
         if drop_values is not None:
@@ -22,9 +23,31 @@ class ColorPointDF():
 
         if color_scheme == "threshold":
 
+            # choose threshold
             if ColorPointDF.use_threshold == "const":
                 threshold = ColorPointDF.threshold_const
-            elif ColorPointDF.use_threshold == "percentage":
+            elif ColorPointDF.use_threshold == "quantile":
+                threshold = df["value"].quantile(ColorPointDF.threshold_quantile)
+            else:
+                raise ValueError("wrong color_scheme value")
+            
+            # filter out values below threshold
+            df = df.loc[df["value"] >= threshold]
+
+            # set colors
+            rgb = ImageColor.getrgb("white")
+            df['R'] = rgb[0]
+            df['G'] = rgb[1]
+            df['B'] = rgb[2]
+
+
+
+        return df
+
+
+
+
+
 
 
 
