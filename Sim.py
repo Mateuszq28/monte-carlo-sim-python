ad43a42c5773aea7bd55582991ffb24932367ce5
Mateusz Miler <mateuszq28@gmail.com>
2023-09-07 17:25:58 +0200

    debug

--- Sim.py
+++ Sim.py
@@ -205,7 +205,7 @@ class Sim():
                             else:
                                 flag_reflect = True
                             
-                            if flag_reflect:
+                            if not flag_reflect:
                                 # ONLY REFLECTION (OLD RAY)
                                 # photon weight is not changed
                                 photon.pos = boundary_pos
