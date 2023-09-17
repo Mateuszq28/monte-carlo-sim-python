0850ee6b8d073902c2423c9cea18f07f89284c0a
Mateusz Miler <mateuszq28@gmail.com>
2023-09-17 02:56:19 +0200

    change random generator

--- FeatureSampling.py
+++ FeatureSampling.py
@@ -9,7 +9,11 @@ from scipy.special import erf
 # - number of generated random numbers: 3262
 # - number of seperate random generator instances (MyRandom): 25
 
-class MyRandom():
+class MyRandom_old():
+    """
+    by default it uses Mersenne Twister (MT19937)
+    generator period = 2^19937 = 4,3E6001
+    """
 
     # random seed to be set in next instance of MyRandom class
     random_state_pool = 0
@@ -42,6 +46,45 @@ class MyRandom():
         """
         MyRandom.generated_num += 1
         return self.rng1.randint(low=low, high=high, size=size)
+    
+
+class MyRandom():
+    """
+    by default it uses Permuted Congruential Generator (64-bit, PCG64)
+    generator period = 2^128 = 3,4E38
+    """
+
+    # random seed to be set in next instance of MyRandom class
+    random_state_pool = 0
+    # how many random numbers has been already generated
+    generated_num = 0
+
+    def __init__(self):
+        self.random_state = MyRandom.random_state_pool
+        MyRandom.random_state_pool += 1
+        self.rng1 = np.random.default_rng(seed=self.random_state)
+
+    def uniform_closed(self, low: int, high: int, precision):
+        """
+        Generate random float number from closed interval [a, b]
+        """
+        rnd = self.rng1.integers(0, (high-low) * (10 ** precision) + 1) / (10 ** precision) + low
+        MyRandom.generated_num += 1
+        return rnd
+    
+    def uniform_half_open(self, low, high):
+        """
+        Generate random float number from half-open interval [a, b)
+        """
+        MyRandom.generated_num += 1
+        return self.rng1.uniform(low=low, high=high)
+    
+    def randint(self, low, high, size=None):
+        """
+        Generate random int from half-open interval [a, b)
+        """
+        MyRandom.generated_num += 1
+        return self.rng1.integers(low=low, high=high, size=size)
         
 
 class FeatureSampling():
