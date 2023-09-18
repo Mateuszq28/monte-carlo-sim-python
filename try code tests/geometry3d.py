629220b82bfd15e3c5667a2e3604a5828c8b6e78
Mateusz Miler <mateuszq28@gmail.com>
2023-09-18 08:06:04 +0200

    move experiments to separate file

---
+++ try code tests/geometry3d.py
@@ -0,0 +1,43 @@
+import Geometry3D
+
+# makeMaterials = MakeMaterial()
+# cuboid = makeMaterials.cuboid(label=0, start_p=(0,0,0), end_p=(5,5,5), propEnvShape=None)
+
+# p_to_check = [(2,2,2), (2,5,2), (5,5,5), (7,7,7), (0,0,7)]
+# p_to_check = [Geometry3D.Point(p) for p in p_to_check]
+# p_to_check_names = ["point_in", "point_boundary", "point_vert", "point_out", "point_out2"]
+
+# for p, name in zip(p_to_check, p_to_check_names):
+#     # not working
+#     # dist = cuboid.distance(p)
+#     # print(name, dist)
+
+#     p_segm_start = Geometry3D.Point(10,10,10)
+#     seg = Geometry3D.Segment(p_segm_start, p)
+#     intersect = cuboid.intersection(seg)
+#     print(name, intersect)
+
+#     # not working
+#     # dist = cuboid.distance(seg)
+#     # print(name, dist)
+                
+
+
+
+
+box = Geometry3D.Parallelepiped(Geometry3D.Point(0,0,0), Geometry3D.Vector(1,0,0), Geometry3D.Vector(0,1,0), Geometry3D.Vector(0,0,1))
+polygons = box.convex_polygons
+print()
+print(polygons)
+print()
+planes = [pol.plane.n for pol in polygons]
+print(planes)
+print()
+
+p_list = [Geometry3D.Point(0,0,0), Geometry3D.Point(0,0,1), Geometry3D.Point(0,1,0), Geometry3D.Point(0,1,1)]
+polygon2 = Geometry3D.ConvexPolygon(pts=p_list)
+print(polygon2.plane.n)
+print()
+
+
+polygon2.center_point
