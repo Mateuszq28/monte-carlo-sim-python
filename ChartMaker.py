a324a5d1da9bab0cdab058135e1e2bd843f108a5
Mateusz Miler <mateuszq28@gmail.com>
2023-10-13 22:42:40 +0200

    for experiments

--- ChartMaker.py
+++ ChartMaker.py
@@ -82,9 +82,9 @@ class ChartMaker():
 
 
         # MAKE AND SHOW OBJECT THAT CONTAIN MATERIAL LABELS + MARKED LIGHT SOURCES LOCATIONS
-        # ChartMaker.show_simulation_preview_DF(propSetup = propSetup,
-        #                                       cs_material="solid",
-        #                                       cs_light_source="solid")
+        ChartMaker.show_simulation_preview_DF(propSetup = propSetup,
+                                              cs_material="solid",
+                                              cs_light_source="solid")
 
         # SHOW PHOTON WEIGHTS (RESULT ENV) + PROP ENV (MATERIAL LABELS)
         # ChartMaker.show_simulation_result_preview_DF(propSetup=propSetup,
@@ -105,8 +105,8 @@ class ChartMaker():
 
         # SHOW RESULT RECORDS
         sl = list(range(10,20)) + list(range(30,40))
-        sl = None
         sl = list(range(10,15))
+        sl = None
         sl = [4, 48]
         sl = [0, 16, 22, 38, 50, 68, 75, 78, 79, 91, 97, 98]
         sl = list(range(0,100))
@@ -174,59 +174,59 @@ class ChartMaker():
 
 
 
-        # [FROM RECORDS] PROJECTIONS + MAKING .PNG IMAGES
-        sh = propSetup.resultShape
-        local_color_scheme = color_scheme
-        local_color_scheme = "photonwise"
-        drop_values = [0, 0.0]
-        drop_values = None
-        local_reset_colors = local_color_scheme
-        local_reset_colors = None
-        ChartMaker.projections_from_resultRecords(resultRecords = propSetup.resultRecords,
-                                                  input_shape = sh,
-                                                  color_scheme = local_color_scheme,
-                                                  drop_values = drop_values,
-                                                  select_photon_id = None,
-                                                  photon_register = propSetup.photon_register,
-                                                  select_parent = True,
-                                                  select_child = True,
-                                                  border_limits = [0, sh[0], 0, sh[1], 0, sh[2]],
-                                                  png_dir = None,
-                                                  sum_same_idx = False,
-                                                  sum_axis = False,
-                                                  reset_png_colors = None,
-                                                  show = True,
-                                                  title_prefix = "",
-                                                  do_connect_lines = do_connect_lines,
-                                                  reset_colors = local_reset_colors,
-                                                  color_points_by_root = color_points_by_root,
-                                                  color_arrows_by_root = color_arrows_by_root)
+        # # [FROM RECORDS] PROJECTIONS + MAKING .PNG IMAGES
+        # sh = propSetup.resultShape
+        # local_color_scheme = color_scheme
+        # local_color_scheme = "photonwise"
+        # drop_values = [0, 0.0]
+        # drop_values = None
+        # local_reset_colors = local_color_scheme
+        # local_reset_colors = None
+        # ChartMaker.projections_from_resultRecords(resultRecords = propSetup.resultRecords,
+        #                                           input_shape = sh,
+        #                                           color_scheme = local_color_scheme,
+        #                                           drop_values = drop_values,
+        #                                           select_photon_id = None,
+        #                                           photon_register = propSetup.photon_register,
+        #                                           select_parent = True,
+        #                                           select_child = True,
+        #                                           border_limits = [0, sh[0], 0, sh[1], 0, sh[2]],
+        #                                           png_dir = None,
+        #                                           sum_same_idx = False,
+        #                                           sum_axis = False,
+        #                                           reset_png_colors = None,
+        #                                           show = True,
+        #                                           title_prefix = "",
+        #                                           do_connect_lines = do_connect_lines,
+        #                                           reset_colors = local_reset_colors,
+        #                                           color_points_by_root = color_points_by_root,
+        #                                           color_arrows_by_root = color_arrows_by_root)
         
-        if sl is not None:
-            take_group = 1
-            for i in range(len(sl)+1-take_group):
-                select_photon_id = [sl[i]]
-                select_photon_id = sl[i:i+take_group]
-                local_color_scheme = "photonwise"
-                ChartMaker.projections_from_resultRecords(resultRecords = propSetup.resultRecords,
-                                                          input_shape = sh,
-                                                          color_scheme = local_color_scheme,
-                                                          drop_values = None,
-                                                          select_photon_id = select_photon_id,
-                                                          photon_register = propSetup.photon_register,
-                                                          select_parent = True,
-                                                          select_child = True,
-                                                          border_limits = [0, sh[0], 0, sh[1], 0, sh[2]],
-                                                          png_dir = os.path.join("slice_img", "single_photon_projection_img"),
-                                                          sum_same_idx = False,
-                                                          sum_axis = False,
-                                                          reset_png_colors = None,
-                                                          show = True,
-                                                          title_prefix = "({}) ".format(i),
-                                                          do_connect_lines = do_connect_lines,
-                                                          reset_colors = local_reset_colors,
-                                                          color_points_by_root = color_points_by_root,
-                                                          color_arrows_by_root = color_arrows_by_root)
+        # if sl is not None:
+        #     take_group = 1
+        #     for i in range(len(sl)+1-take_group):
+        #         select_photon_id = [sl[i]]
+        #         select_photon_id = sl[i:i+take_group]
+        #         local_color_scheme = "photonwise"
+        #         ChartMaker.projections_from_resultRecords(resultRecords = propSetup.resultRecords,
+        #                                                   input_shape = sh,
+        #                                                   color_scheme = local_color_scheme,
+        #                                                   drop_values = None,
+        #                                                   select_photon_id = select_photon_id,
+        #                                                   photon_register = propSetup.photon_register,
+        #                                                   select_parent = True,
+        #                                                   select_child = True,
+        #                                                   border_limits = [0, sh[0], 0, sh[1], 0, sh[2]],
+        #                                                   png_dir = os.path.join("slice_img", "single_photon_projection_img"),
+        #                                                   sum_same_idx = False,
+        #                                                   sum_axis = False,
+        #                                                   reset_png_colors = None,
+        #                                                   show = True,
+        #                                                   title_prefix = "({}) ".format(i),
+        #                                                   do_connect_lines = do_connect_lines,
+        #                                                   reset_colors = local_reset_colors,
+        #                                                   color_points_by_root = color_points_by_root,
+        #                                                   color_arrows_by_root = color_arrows_by_root)
 
 
 
@@ -276,7 +276,7 @@ class ChartMaker():
         else:
             df, arrows_DF = propSetup.make_preview_DF(cs_material, cs_light_source)
             vis = ByVispy()
-            vis.show_ColorPointDF(df, title="simulation preview - propagation env + light sources", connect_lines=arrows_DF, draw_plane_triangles=False)
+            vis.show_ColorPointDF(df, title="simulation preview - propagation env + light sources", connect_lines=None, draw_plane_triangles=False)
 
 
     @staticmethod
